8092e032967774e1ab9b37820742ce6a774e44ac
naoya <naoya0517.mc@gmail.com>
2023-12-07 11:36:41 +0900

    Initial Commit

---
+++ CarDiaryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarDiaryApp
+//
+//  Created by naoya on 2023/12/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
